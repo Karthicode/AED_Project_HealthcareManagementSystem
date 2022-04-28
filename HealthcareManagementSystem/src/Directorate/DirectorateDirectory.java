09e2beb8c6c966fde0684c1247e1d26e9ebf8662
Rishi Gupta <gupta.rishi1@northeastern.edu>
2022-04-28 19:53:16 -0400

    Directorate classes

--- HealthcareManagementSystem/src/Directorate/DirectorateDirectory.java
+++ HealthcareManagementSystem/src/Directorate/DirectorateDirectory.java
@@ -7,4 +7,28 @@ package HealthCareSystem.Directorate;
 
 import java.util.HashMap;
 
+/**
+ *
+ * @author Rishi
+ */
+public class DirectorateDirectory {
+    
+    HashMap<String, Directorate> directorateList = new HashMap<>();
 
+    public HashMap<String, Directorate> getDirectorateList() {
+        return directorateList;
+    }
+
+    public void setDirectorateList(HashMap<String, Directorate> directorateList) {
+        this.directorateList = directorateList;
+    }
+    public DirectorateDirectory() {
+    }
+
+    public void addDirectorate(String user_id, Directorate accountant) {
+        directorateList.put(user_id, accountant);
+    }
+
+    
+    
+}
