34a70188e4d0e9bdb56b389c41bd69df1ca37732
Harshith Narahari <narahari.h@northeastern.edu>
2022-04-28 13:41:35 -0400

    test

--- HealthcareManagementSystem/src/healthcaremanagementsystem/HealthcareManagementSystem.java
+++ HealthcareManagementSystem/src/healthcaremanagementsystem/HealthcareManagementSystem.java
@@ -15,6 +15,7 @@ public class HealthcareManagementSystem {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //testing functionality
     }
     
 }
