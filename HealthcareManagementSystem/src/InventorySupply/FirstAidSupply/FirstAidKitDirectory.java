b6d3c06617248535e35ab60fba079b4ea2ffeb1a
Rishi Gupta <gupta.rishi1@northeastern.edu>
2022-04-29 14:15:01 -0400

    Adding class First aid directory

--- HealthcareManagementSystem/src/InventorySupply/FirstAidSupply/FirstAidKitDirectory.java
+++ HealthcareManagementSystem/src/InventorySupply/FirstAidSupply/FirstAidKitDirectory.java
@@ -12,3 +12,32 @@ import java.util.HashMap;
  *
  * @author Rishi
  */
+
+public class FirstAidKitDirectory {
+    
+    ArrayList<FirstAidKit> firstAidKits = new ArrayList<>();
+
+    public ArrayList<FirstAidKit> getFirstAidKits() {
+        return firstAidKits;
+    }
+
+    public void setFirstAidKits(ArrayList<FirstAidKit> firstAidKits) {
+        this.firstAidKits = firstAidKits;
+    }
+    
+    public void addFirstAidKits(FirstAidKit firstAid){
+        firstAidKits.add(firstAid);
+    }
+    
+    public FirstAidKit findMedSupEquipByName(String first_Aid_Kit_Name){
+        for(FirstAidKit medSupEquip: firstAidKits){
+            if(medSupEquip.getFirst_Aid_Name().equals(first_Aid_Kit_Name))
+                return medSupEquip;
+        }
+        return null;
+    }
+
+   
+    
+    
+}
