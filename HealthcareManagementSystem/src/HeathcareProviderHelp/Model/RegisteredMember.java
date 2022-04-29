a59fc8219dd061dc9496660a99dad8dd08625ebe
Rishi Gupta <gupta.rishi1@northeastern.edu>
2022-04-29 01:43:19 -0400

    Adding few details in class Register Member in folder Health care Provider help

--- HealthcareManagementSystem/src/HeathcareProviderHelp/Model/RegisteredMember.java
+++ HealthcareManagementSystem/src/HeathcareProviderHelp/Model/RegisteredMember.java
@@ -8,6 +8,34 @@ package HeathcareProviderHelp.Model;
  *
  * @author Rishi
  */
+public class RegisteredMember {
+    
+    
+   private String HelperGroupId;
+   private String QualifiedMemberName;
+    
+    
+    public String getHelperGroupId() {
+        return HelperGroupId;
+    }
+
+    public void setHelperGroupId(String HelperGroupId) {
+        this.HelperGroupId = HelperGroupId;
+    }
+
+    public String getQualifiedMemberName() {
+        return QualifiedMemberName;
+    }
+
+    public void setQualifiedMemberName(String QualifiedMemberName) {
+        this.QualifiedMemberName = QualifiedMemberName;
+    }
+    
+    
+    @Override
+    public String toString(){
+        return HelperGroupId;
+    }
 
     
 }
