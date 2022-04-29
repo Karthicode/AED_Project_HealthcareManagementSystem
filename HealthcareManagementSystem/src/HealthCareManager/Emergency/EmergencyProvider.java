faf688c1b202f3d447f096087ee2ccac66ba3fd3
Harshith Narahari <narahari.h@northeastern.edu>
2022-04-29 01:50:21 -0400

    coding emergency provider class

---
+++ HealthcareManagementSystem/src/HealthCareManager/Emergency/EmergencyProvider.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HealthCareManager.Emergency;
+
+import IntegrationSystem.CustomerAccount;
+
+/**
+ *
+ * @author harshithnarahari
+ */
+public class EmergencyProvider {
+    
+//    CustomerAccount userAccount;
+    private String emergencyProviderId;
+    private String emergencyProviderName;
+    
+    public String getEmergencyProviderId() {
+        return emergencyProviderId;
+    }
+
+    
+
+    
+}
