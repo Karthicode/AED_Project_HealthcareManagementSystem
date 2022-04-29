ccd988bf49fdbca561cb666ef41d4a5a6b4b9c6a
Rishi Gupta <gupta.rishi1@northeastern.edu>
2022-04-29 14:17:34 -0400

    Adding class Helper team model

--- HealthcareManagementSystem/src/model/HelperTeam/HelperTeamModel.java
+++ HealthcareManagementSystem/src/model/HelperTeam/HelperTeamModel.java
@@ -11,3 +11,46 @@ import java.util.ArrayList;
  *
  * @author Rishi
  */
+
+public class HelperTeamModel {
+    
+    ArrayList<HelperTeam> customerSupportTeamList = new ArrayList<>();
+
+    public ArrayList<HelperTeam> getCustomerSupportTeamList() {
+        return customerSupportTeamList;
+    }
+
+    public void setCustomerSupportTeamList(ArrayList<HelperTeam> customerSupportTeamList) {
+        this.customerSupportTeamList = customerSupportTeamList;
+    }
+    public HelperTeamModel() {
+    }
+
+    public void addCustomerSupportTeam(HelperTeam customerSupportTeam) {
+        customerSupportTeamList.add(customerSupportTeam);
+    }
+
+    public void updateCustomerSupportTeam(ArrayList<String> user_input, HelperTeam updated_customerSupportTeam) {
+        
+        for(HelperTeam customerSupportTeam: customerSupportTeamList)
+        {
+            if(customerSupportTeam.getHelperTeamId().equals(updated_customerSupportTeam.getHelperTeamId()))
+            {   
+                customerSupportTeam.setName_Helper_Team(user_input.get(1));
+                break;
+            }
+        }
+    }
+
+    public void deleteCustomerSupportTeam(HelperTeam selected_customerSupportTeam) {
+        for(HelperTeam customerSupportTeam: customerSupportTeamList)
+        {
+            if(customerSupportTeam.getHelperTeamId().equals(selected_customerSupportTeam.getHelperTeamId()))
+            {   
+                customerSupportTeamList.remove(customerSupportTeam);
+                break;
+            }
+        }
+    }
+    
+}
