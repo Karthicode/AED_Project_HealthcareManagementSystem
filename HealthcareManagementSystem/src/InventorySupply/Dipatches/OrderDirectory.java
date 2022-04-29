baebf90e59d7d5683fe7edba0a7f0238ab398fd4
Rishi Gupta <gupta.rishi1@northeastern.edu>
2022-04-29 14:08:41 -0400

    Adding class Order Directory

--- HealthcareManagementSystem/src/InventorySupply/Dipatches/OrderDirectory.java
+++ HealthcareManagementSystem/src/InventorySupply/Dipatches/OrderDirectory.java
@@ -12,3 +12,35 @@ import java.util.HashMap;
  *
  * @author Rishi
  */
+
+
+public class OrderDirectory {
+    
+    ArrayList<DispatchRequest> orderList = new ArrayList<>();
+    String last_order_id = "ORDER001";
+    public String getLast_order_id() {
+        return last_order_id;
+    }
+
+    public void setLast_order_id(String last_order_id) {
+        this.last_order_id = last_order_id;
+    }
+    
+    public ArrayList<DispatchRequest> getOrderList() {
+        return orderList;
+    }
+
+    public void setOrderList(ArrayList<DispatchRequest> orderList) {
+        this.orderList = orderList;
+    }
+    
+    public void addOrder(DispatchRequest order){
+        orderList.add(order);
+    }
+    
+   
+
+   
+    
+    
+}
