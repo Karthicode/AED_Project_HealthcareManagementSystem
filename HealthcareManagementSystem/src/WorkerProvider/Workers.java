129d688830fc97c02093997cfd4d058bf5d55026
Harshith Narahari <narahari.h@northeastern.edu>
2022-04-29 13:52:48 -0400

    adding  workers class files

---
+++ HealthcareManagementSystem/src/WorkerProvider/Workers.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package WorkerProvider;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author harshithnarahari
+ * 
+ */
+public class Workers {
+
+    private ArrayList<Worker> workerList;
+
+    public Workers() {
+        workerList = new ArrayList();
+    }
+  
+}
