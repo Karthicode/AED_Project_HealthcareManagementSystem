6abdcf6a69cdae6b6e87931b87a97df77af56efc
Harshith Narahari <narahari.h@northeastern.edu>
2022-04-30 15:03:45 -0400

    adding enterprise structure

---
+++ HealthcareManagementSystem/src/IntegrationSystem/SetEnterpriseStructure.java
@@ -0,0 +1,20 @@
+package IntegrationSystem;
+
+import WorkerProvider.Worker;
+import WorkerProvider.Workers;
+import HealthCareManager.PartnerInfirmaries.PartnerInfirmiries;
+import HealthCareManager.PartnerInfirmaries.PartnerInfirmiriesDirectory;
+import HealthCareMainSystem.AdminRoles.PartnerAdminRole;
+import HealthCareMainSystem.AdminRoles.SystemAdminRole;
+
+/**
+ *
+ * @author harshithnarahari
+ * 
+ */
+
+public class SetEnterpriseStructure {
+ 
+    }
+    
+}
